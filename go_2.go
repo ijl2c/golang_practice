9f0713c5e61ee6326a7c78f584a11a068d338960
aj1993 <adityajayanti1993@gmail.com>
2022-09-26 20:05:16 +0200

    Basic calculator program. Using functions

--- go_2.go
+++ go_2.go
@@ -1,7 +1,25 @@
+// Basic calculator program
+
 package main
 
-import "fmt"
+import ("fmt")
+
+func add(x int, y int) int{
+	return x+y
+}
+
+func sub(x int, y int) int{
+	return x-y
+}
+
+func mul(x int, y int) int{
+	return x*y
+}
+
+func divide(x int, y int)int{
+	return x/y
+}
 
 func main(){
-	fmt.Println("From the second one!!")
+	fmt.Println("Addition:",add(10, 5), "Sub:",sub(10, 5), "Mul:",mul(10,5), "Div:",divide(10,5))
 }
